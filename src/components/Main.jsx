9dac279d8b434c32b2b7df71156731f49dc27d4b
Diego Polanco <diegopolanco1969@icloud.com>
2023-07-05 19:43:45 -0500

    fixbug

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,21 +1,20 @@
-import link from "../data/data.json"
+import link from "/public/data/data.json";
 
 const textColor = {
   reaction: "text-lightRed",
   memory: "text-orangeyYellow",
   verbal: "text-greenTeal",
-  visual: "text-cobaltBlue"
-}
+  visual: "text-cobaltBlue",
+};
 
 const bgColor = {
   reaction: "bg-[#fff0f0]",
   memory: "bg-[#fffbf5]",
   verbal: "bg-[#f5fffd]",
-  visual: "bg-[#f6f6fe]"
-}
+  visual: "bg-[#f6f6fe]",
+};
 
 const Main = () => {
-
   return (
     <section className="px-5">
       <h3 className="py-5">Summary</h3>
@@ -23,21 +22,32 @@ const Main = () => {
         {link.map((data) => (
           <div
             key={data.id}
-            className={`${bgColor[data.category]} px-4 py-[10px] rounded-lg flex justify-between hover:scale-105 hover:transition-all cursor-pointer shadow-sm`}
+            className={`${
+              bgColor[data.category]
+            } px-4 py-[10px] rounded-lg flex justify-between hover:scale-105 hover:transition-all cursor-pointer shadow-sm`}
           >
             <div className="flex gap-2 ">
               <div>
                 <img src={data.icon} alt="" />
               </div>
-              <h3 className={`${textColor[data.category]} capitalize text-[14px] font-bold`}>{data.category}</h3>
+              <h3
+                className={`${
+                  textColor[data.category]
+                } capitalize text-[14px] font-bold`}
+              >
+                {data.category}
+              </h3>
             </div>
-            <p className= "font-bold">{data.score} <span className="text-darkGrayBlue">/ 100</span></p>
+            <p className="font-bold">
+              {data.score} <span className="text-darkGrayBlue">/ 100</span>
+            </p>
           </div>
         ))}
-        <button className="bg-[#303b5a] hover:transition-all hover:scale-105 hover:bg-gradient-to-b from-lightSlateBlue to-lightRoyalBlue  text-primary capitalize px-4 py-[10px] rounded-3xl mb-2 sm:mb-5">continue</button>
+        <button className="bg-[#303b5a] hover:transition-all hover:scale-105 hover:bg-gradient-to-b from-lightSlateBlue to-lightRoyalBlue  text-primary capitalize px-4 py-[10px] rounded-3xl mb-2 sm:mb-5">
+          continue
+        </button>
       </article>
     </section>
-
   );
 };
 export default Main;
